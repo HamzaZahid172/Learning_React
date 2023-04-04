5b2393f1f9a6b776e4aefc626ce8ac4b4143c56a
HamzaZahid172 <zahidhamza172@gmail.com>
2023-04-05 02:44:45 +0500

    main Updated some text in APP.js file & also removed some unused code in index.js file

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h2> Let's get started</h2>
+      <p>Hello my name is Hamza Zahid. I'm a Software Engineer at Arbisoft</p>
+    </div>
+  );
+}
+
+export default App;
