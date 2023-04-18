6bce9e88b4156471c9d319fc3c174719c56ba87e
HamzaZahid172 <zahidhamza172@gmail.com>
2023-04-19 02:46:21 +0500

    Listening to Event & Event Handler

--- react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -2,12 +2,16 @@ import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
 import "../Expenses/ExpenseItem.css";
 const ExpenseItem = (props) => {
+  const clickButton = () => {
+    console.log('Clicked!!!!!');
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <p className="expense-item__price">$ {props.amount}</p>
+        <button  onClick={clickButton}></button>
       </div>
     </Card>
   );
