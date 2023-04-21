e4866dd38b831fe9874d2c594735aef403227873
HamzaZahid172 <zahidhamza172@gmail.com>
2023-04-22 01:37:23 +0500

    Working with multiple states

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -1,8 +1,21 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enterTitle, setEnterTitle] = useState("");
+  const [enterAmount, setEnterAmount] = useState("");
+  const [enterDate, setEnterDate] = useState("");
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
+    setEnterTitle(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnterAmount(event.target.value);
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnterDate(event.target.value);
   };
 
   return (
@@ -14,11 +27,21 @@ const ExpenseForm = () => {
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2023-12-30" />
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2023-12-30"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__action">
